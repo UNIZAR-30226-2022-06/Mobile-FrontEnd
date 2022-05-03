9fe186cc6fd27effbc5cbb9abf14052efbe131bb
Ibón González Martínez de Apellániz <igonzalezma7@gmail.com>
2022-05-03 04:19:31 +0200

    Version 2022.05.02

--- itreader/app/src/main/java/com/softkare/itreader/fragments/SearchFragment.kt
+++ itreader/app/src/main/java/com/softkare/itreader/fragments/SearchFragment.kt
@@ -5,15 +5,61 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.softkare.itreader.R
+import com.softkare.itreader.adapter.bookAdapter
 
 class SearchFragment : Fragment() {
+    lateinit var list : MutableList<Book>
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        val view = inflater.inflate(R.layout.fragment_search, container, false)
+        val searchView = view.findViewById<SearchView>(R.id.searchview)
+        val recyclerView : RecyclerView = view.findViewById(R.id.recyclerSearch)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = bookAdapter(BookProvider.bookList)
+        list = mutableListOf()
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                searchView.clearFocus()
+                list.clear()
+                for (b in list) {
+                    // Se busca por el titulo del libro
+                    if (b.name.toLowerCase().contains(p0.toString().toLowerCase())) {
+                        list.add(b)
+                    }
+                    // Tambien busca por el autor del libro
+                    else if (b.author.toLowerCase().contains(p0.toString().toLowerCase())) {
+                        list.add(b)
+                    }
+                    recyclerView.adapter = bookAdapter(list)
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                searchView.clearFocus()
+                list.clear()
+                for (b in list) {
+                    if (b.name.toLowerCase().contains(p0.toString().toLowerCase())) {
+                        list.add(b)
+                    }
+                    // Tambien busca por el autor del libro
+                    else if (b.author.toLowerCase().contains(p0.toString().toLowerCase())) {
+                        list.add(b)
+                    }
+                    recyclerView.adapter = bookAdapter(list)
+                }
+                return false
+            }
+        })
+
+        return view
     }
 
 
